727ed05a815e57ab51170cc750321a7f3bcf4e24
Tianqi <your-email@example.com>
2026-02-13 13:53:34 +0000

    Week 3 - category page and slug implemented

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category).order_by('-views')
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
