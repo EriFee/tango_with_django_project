8f7ebf68e2460cae4684719778e133240deb7de2
Tianqi <your-email@example.com>
2026-02-13 15:08:54 +0000

    Chapter 8 authentication completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Category, Page
+from rango.models import Category, Page, UserProfile
 
+admin.site.register(UserProfile)
 admin.site.register(Category)
 admin.site.register(Page)
--- rango/forms.py
+++ rango/forms.py
@@ -9,3 +9,18 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         fields = ('title', 'url', 'views')
+
+from django.contrib.auth.models import User
+from rango.models import UserProfile
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
